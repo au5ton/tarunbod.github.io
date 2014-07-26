e6a5074fdd340992279daaa10c38d45440b48d6d
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-26 10:58:01 -0400

    Did some fancy css positioning

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $("button").button();
     $("#splash").text(splashes.getSplash());
     if ($("#splash").text() === "Animation!") {
-      $("#splash").css({"-webkit-animation": "animation 500ms linear infinite", "animation" : "animation 500ms linear infinite"});
+      $("#splash").css({"-webkit-animation": "animation 1s linear infinite, splash 500ms linear infinite", "animation" : "animation 1s linear infinite, splash 500ms linear infinite"});
     }
 });
