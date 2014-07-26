97a3beb2ded1d31d1e6f775110558ebc6df0072a
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-26 18:48:32 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $("button").button();
     $("#splash").text(splashes.getSplash());
-<<<<<<< HEAD
     setInterval(updateSplash, 1);
 });
 
@@ -13,9 +12,7 @@ function updateSplash() {
     $("#splash").css({"-webkit-animation": "splash 500ms linear infinite", "animation" : "splash 500ms linear infinite"});
   }
 }
-=======
     if ($("#splash").text() === "Animation!") {
       $("#splash").css({"-webkit-animation": "splash 500ms linear infinite, animation 1s linear infinite", "animation" : "animation 1s linear infinite, splash 500ms linear infinite"});
     }
 });
->>>>>>> 0f9d1b2ae17c4c8c6d9177c6e90a6ab081d5409e
