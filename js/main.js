64d2b1704f1e8bd7e5b12c76dae3e6bf23b6ea5c
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-26 18:49:53 -0400

    update

--- js/main.js
+++ js/main.js
@@ -15,4 +15,4 @@ function updateSplash() {
     if ($("#splash").text() === "Animation!") {
       $("#splash").css({"-webkit-animation": "splash 500ms linear infinite, animation 1s linear infinite", "animation" : "animation 1s linear infinite, splash 500ms linear infinite"});
     }
-});
+}
