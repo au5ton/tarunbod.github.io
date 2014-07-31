b7a894011857f67890324fc1d348700dfc4006f3
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-31 11:12:53 -0400

    Added Link Bar!

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     $("button").button();
     $("#splash").text(splashes.getSplash());
     $("#container").children().click(function(e) {e.stopPropagation()});
+    $("#linkBar").click(switchTheme);
     $("#container").click(switchTheme);
+    setTimeout(function() {window.scrollTo(0, 83);}, 10);
     setInterval(updateSplash, 1);
 });
 
@@ -19,11 +21,15 @@ function updateSplash() {
 function switchTheme() {
   if (dark) {
     $("#container").css("background-color", "rgba(255,255,255,0.7);");
+    $("#linkBar").css("background-color", "rgba(255,255,255,0.7);");
     $("#container").css("color", "black");
+    $("#linkBar").css("color", "black");
     dark = false;
   } else {
     $("#container").css("background-color", "rgba(0,0,0,0.7);");
+    $("#linkBar").css("background-color", "rgba(0,0,0,0.7);");
     $("#container").css("color", "white");
+    $("#linkBar").css("color", "white");
     dark = true;
   }
 } 
