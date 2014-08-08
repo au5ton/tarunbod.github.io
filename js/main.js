734971682e7104179a537b878d8ff35b2c6ffc51
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-08-08 11:49:15 -0400

    Bootstrap and NavBar!

--- js/main.js
+++ js/main.js
@@ -1,36 +1,22 @@
 $(document).ready(function() {
     $("button").button();
-    $("#splash").text(splashes.getSplash());
+    splashes.setSplash();
     $("#container").children().click(function(e) {e.stopPropagation()});
     $("#linkBar").children().click(function(e) {e.stopPropagation()});
     $("#linkBar").click(switchTheme);
     $("#container").click(switchTheme);
-    $("#menuBtn").click(function() {$("#linkBar").toggle("slide")});
-    setInterval(updateSplash, 1);
 });
 
 var dark = true;
 
-function updateSplash() {
-  if ($("#splash").text() === "Animation!") {
-      $("#splash").css({"-webkit-animation": "animation 1s linear infinite, splash 500ms linear infinite", "animation" : "animation 1s linear infinite, splash 500ms linear infinite"});
-  } else {
-    $("#splash").css({"-webkit-animation": "splash 500ms linear infinite", "animation" : "splash 500ms linear infinite"});
-  }
-}
-
 function switchTheme() {
   if (dark) {
-    $("#container").css("background-color", "rgba(255,255,255,0.7);");
-    $("#linkBar").css("background-color", "rgba(255,255,255,0.7);");
-    $("#container").css("color", "black");
-    $("#linkBar").css("color", "black");
+    $(".bgswitch").css({"background-color": "rgba(255,255,255,0.7);", "color": "black"});
+    // $("#cssmenu ul li a:before").css("background-color: rgba(255,255,255,0.7);");
     dark = false;
   } else {
-    $("#container").css("background-color", "rgba(0,0,0,0.7);");
-    $("#linkBar").css("background-color", "rgba(0,0,0,0.7);");
-    $("#container").css("color", "white");
-    $("#linkBar").css("color", "white");
+    $(".bgswitch").css({"background-color": "rgba(0,0,0,0.7);", "color": "white"});
+    // $("#cssmenu ul li a:before").css("background-color: rgba(0,0,0,0.7);");
     dark = true;
   }
 } 
--- js/splashes.js
+++ js/splashes.js
@@ -1,18 +1,33 @@
 var splashes = {
 	list: [
 		"Uses jQuery!",
+		"Uses Bootstrap!",
 		"There's no place like 127.0.0.1!",
 		"window.alert(\"Hello, World!\")",
 		"HTML5!",
 		"CSS3!",
 		"PHP Free!",
-		"Animation!"
+		"Animation!",
+		"WORA!",
+		"Java!",
+		"Tiny!"
 	],
 	getSplash: function () {
 		var index = Math.floor(Math.random() * this.list.length);
 		return this.list[index];
 	},
 	setSplash: function() {
-		$("#splash").text(this.getSplash());
+		var splash = this.getSplash();
+		if (splash === "Animation!") {
+			$("#splash").css({"-webkit-animation": "animation 1s linear infinite, splash 500ms linear infinite", "animation" : "animation 1s linear infinite, splash 500ms linear infinite"});
+		} else {
+			if (splash === "Tiny!") {
+				$("#splash").css("font-size", "8px");
+			} else {
+				$("#splash").css("font-size", "24px");
+			}
+			$("#splash").css({"-webkit-animation": "splash 500ms linear infinite", "animation" : "splash 500ms linear infinite"});
+		}
+		$("#splash").text(splash);
 	}
 }
