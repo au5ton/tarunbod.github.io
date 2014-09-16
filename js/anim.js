742eab51faa46992d764c14ffb0cc9157a1805ff
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-09-16 18:38:54 -0400

    Kind of update

--- js/anim.js
+++ js/anim.js
@@ -1,5 +1,3 @@
-var showProjectsAnimation = true;
-
 var titleBounce = new Bounce();
 titleBounce.translate({
 	from: {x: 300, y: -300},
@@ -17,20 +15,54 @@ headerBounce.scale({
 });
 
 var infoBounce = new Bounce();
-infoBounce.scale({
-	from: {x: 0, y: 0},
-	to: {x: 1, y: 1},
+infoBounce.translate({
+	from: {x: -500, y: 0},
+	to: {x: 0, y: 0},
 	duration: 1000,
 	stiffness: 2
 });
 
 var projectsBounce = new Bounce();
-projectsBounce.scale({
-	from: {x: 0, y: 0},
-	to: {x: 1, y: 1},
+projectsBounce.translate({
+	from: {x: 0, y: 500},
+	to: {x: 0, y: 0},
 	duration: 1000,
 	stiffness: 2
 });
+
+var lightSpeedIn = new Bounce();
+lightSpeedIn.translate({
+	from: {x: -500, y: 0},
+	to: {x: 0, y: 0},
+	duration: 1000,
+	stiffness: 5
+}).skew({
+	from: {x: 20, y: 0},
+	to: {x: 0, y: 0},
+	duration: 1000,
+	stiffness: 5
+});
+
+var scaleUp = new Bounce();
+scaleUp.scale({
+	from: {x: 1, y: 1},
+	to: {x: 5, y: 5},
+	duration: 1000,
+	stiffness: 3
+});
+
+var scaleDown = new Bounce();
+scaleDown.scale({
+	from: {x: 10, y: 10},
+	to: {x: 1, y: 1},
+	duration: 1000,
+	stiffness: 3
+})
+
+function showNavbar() {
+	lightSpeedIn.applyTo($(".navbar"));
+	setTimeout(showTitle, 500);
+}
 
 function showTitle() {
 	$(".header, #title").animate({opacity: 1});
@@ -42,10 +74,12 @@ function showTitle() {
 function showInfo() {
 	$("#info").animate({opacity: 1});
 	infoBounce.applyTo($("#info"));
+	if (document.title === "Portfolio") {
+		setTimeout(showProjects, 1000);
+	}
 }
 
 function showProjects() {
 	$(".project").animate({opacity: 1});
 	projectsBounce.applyTo($(".project"));
-	showProjectsAnimation = false;
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    setTimeout(showTitle, 500);
+    showNavbar();
 });
